cd0f1547edd1d3f0a34c5864d2a933b8b4541b2e
Arif Rabbani <arif.93rabbani@gmail.com>
2020-12-14 11:10:35 +0530

    Game component ui designed.

--- src/components/game.js
+++ src/components/game.js
@@ -15,17 +15,59 @@ export default class Game extends React.Component {
     };
   }
 
+  gameStatus = () =>
+    this.state.availableNumbers.length === 0 ? "won" : "active";
+
+  candidatesAreWrong = () =>
+    utils.sum(this.state.candidateNumbers) > this.state.stars;
+
+  numberStatus = (number) => {
+    if (!this.state.availableNumbers.includes(number)) {
+      return "used";
+    }
+
+    if (this.state.candidateNumbers.includes(number)) {
+      return this.candidatesAreWrong() ? "wrong" : "candidate";
+    }
+
+    return "available";
+  };
+
   onNumberClick = (number, status) => {
     console.log(number, status);
   };
 
   render() {
     return (
-      <>
-        <StarDisplay count={this.stars} />
-        <PlayNumber number={1} status={"used"} onClick={this.onNumberClick} />
-        <PlayAgain gameStatus={"lost"} onClick={() => {}} />
-      </>
+      <div className="game">
+        <div className="instruction">
+          Pick 1 or more numbers to make the sum equal to the number of stars
+        </div>
+        <div className="playground">
+          <div className="game-state">
+            {this.gameStatus() !== "active" ? (
+              <PlayAgain
+                gameStatus={this.gameStatus()}
+                onClick={() => {
+                  console.log("PLAY AGAIN");
+                }}
+              />
+            ) : (
+              <StarDisplay count={this.state.stars} />
+            )}
+          </div>
+          <div className="number-display">
+            {utils.range(1, 9).map((number) => (
+              <PlayNumber
+                key={number}
+                number={number}
+                status={this.numberStatus(number)}
+                onClick={this.onNumberClick}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     );
   }
 }
